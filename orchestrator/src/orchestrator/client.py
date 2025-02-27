fd0d7e63983128225ad6ba91034b62993e57ac5b
Luca Samorè <luca.samore.01@gmail.com>
2025-02-27 09:33:07 +0100

    feat(orchestrator): handle multiple messages from the same client

--- orchestrator/src/orchestrator/client.py
+++ orchestrator/src/orchestrator/client.py
@@ -1,13 +1,22 @@
+import os
 from websockets.sync.client import connect
+from dotenv import load_dotenv
+
+load_dotenv()
+
+host = os.getenv("WEBSOCKET_SERVER_HOST")
+port = (lambda p: int(p) if p else None)(os.getenv("WEBSOCKET_SERVER_PORT"))
 
 
-def hello():
-    uri = "ws://localhost:8765"
+def main():
+    uri = f"ws://{host}:{port}"
     with connect(uri) as websocket:
-        websocket.send("Hello from client!")
-        msg = websocket.recv()
-        print(f"FROM Server: {msg}")
+        for i in range(5):
+            print(f"{i + 1}: Hello from client!")
+            websocket.send(f"{i + 1}: Hello from client!")
+            msg = websocket.recv()
+            print(f"FROM Server: {msg}")
 
 
 if __name__ == "__main__":
-    hello()
+    main()
--- orchestrator/src/orchestrator/orchestrator.py
+++ orchestrator/src/orchestrator/orchestrator.py
@@ -1,17 +1,31 @@
 import asyncio
+import os
 from websockets.asyncio.server import serve
+from websockets.exceptions import ConnectionClosedOK
+from dotenv import load_dotenv
+
+load_dotenv()
+
+host = os.getenv("WEBSOCKET_SERVER_HOST")
+port = (lambda p: int(p) if p else None)(os.getenv("WEBSOCKET_SERVER_PORT"))
 
 
-async def hello(websocket):
-    msg = await websocket.recv()
-    print(f"FROM Client: {msg}")
-    await websocket.send("Hello from server!")
+async def handle_incoming_query(websocket):
+    try:
+        async for message in websocket:
+            print(f"FROM Client: {message}")
+            await websocket.send("Hello from server!")
+    except ConnectionClosedOK:
+        print("Connection closed by client")
 
 
 async def main():
-    async with serve(hello, "localhost", 8765) as server:
+    async with serve(handler=handle_incoming_query, host=host, port=port) as server:
         await server.serve_forever()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nServer stopped")
