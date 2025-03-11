834df6fe5f6ba2e6da128f5881a9657e8ecd1d5f
Luca Samorè <luca.samore.01@gmail.com>
2025-03-11 17:36:12 +0100

    refactor(orchestrator): clean up planning module

--- orchestrator/src/orchestrator/planning.py
+++ orchestrator/src/orchestrator/planning.py
@@ -1,31 +1,8 @@
 from .doctors import DiseaseQuestions, get_diseases
 from enum import Enum
 from pydantic import BaseModel, ValidationError
-from kafka import KafkaProducer
-from dotenv import load_dotenv
-from ollama import Client
-from .utilities import logger, PromptTemplate
-import string
-import os
-import json
+from .utilities import logger, PromptTemplate, build_prompt, llm, producer
 
-load_dotenv()
-
-host = os.getenv("CLUSTER_HOST")
-port = (lambda p: int(p) if p else None)(os.getenv("CLUSTER_PORT"))
-
-llm = Client(host=f"http://{host}:{port}")
-
-producer = KafkaProducer(
-    bootstrap_servers=os.getenv('KAFKA_BROKER'),
-    value_serializer=lambda v: json.dumps(v).encode('utf-8'),
-)
-
-
-def build_prompt(template: str, **kwargs) -> str:
-    with open(template, "r") as f:
-        content = f.read()
-    return string.Template(content).substitute(kwargs)
 
 class Grade(Enum):
     EASY = "EASY"
@@ -58,15 +35,11 @@ def act(outcome: ReasoningOutcome, query: str) -> None:
             answer_directly(query)
         case Grade.MEDIUM:
             disease_question = outcome.diseases[0]
-            producer.send(
-                disease_question.disease,
-                { "test": disease_question.question }
-            )
+            producer.send(disease_question.disease, {"test": disease_question.question})
         case Grade.HARD:
             for disease_question in outcome.diseases:
                 producer.send(
-                    disease_question.disease,
-                    { "test": disease_question.question }
+                    disease_question.disease, {"test": disease_question.question}
                 )
 
 
--- orchestrator/src/orchestrator/utilities.py
+++ orchestrator/src/orchestrator/utilities.py
@@ -1,5 +1,13 @@
 from loguru import logger
 from enum import Enum
+from dotenv import load_dotenv
+from kafka import KafkaProducer
+from ollama import Client
+import os
+import json
+import string
+
+load_dotenv()
 
 
 class PromptTemplate(Enum):
@@ -7,3 +15,19 @@ class PromptTemplate(Enum):
 
 
 logger.add("./orchestrator/logs/orchestrator.log", rotation="10 MB")
+
+host = os.getenv("CLUSTER_HOST")
+port = (lambda p: int(p) if p else None)(os.getenv("CLUSTER_PORT"))
+
+llm = Client(host=f"http://{host}:{port}")
+
+producer = KafkaProducer(
+    bootstrap_servers=os.getenv("KAFKA_BROKER"),
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+)
+
+
+def build_prompt(template: str, **kwargs) -> str:
+    with open(template, "r") as f:
+        content = f.read()
+    return string.Template(content).substitute(kwargs)
